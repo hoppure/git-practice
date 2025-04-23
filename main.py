dc42ecb927900c8fd86afa5c9f74448d0ba399dd
skiersong9 <0603paul@gmail.com>
2025-04-23 18:37:14 +0900

    refactor: short code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import play_random
-import play_optimal
+from play_random import play_random
+from play_optimal import play_optimal
 
 if __name__=='__main__':
     n=1000
-    print(play_random.play_random(n))
-    print(play_optimal.play_optimal(n))
+    print(play_random(n))
+    print(play_optimal(n))
