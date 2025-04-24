af7f21639835702e0486b046ffa839416fa8b56c
vivalavidaajy <vivalavidaajy@gmail.com>
2025-04-24 18:05:44 +0900

    feat: Calculator function

---
+++ calculator.py
@@ -0,0 +1,14 @@
+class calculator():
+  def add(a, b):
+    return a + b
+  def sub(a, b):
+    return a - b
+  def mul(a, b):
+    return a * b
+  def div(a, b):
+    return a / b
+
+print(calculator.add(1, 5))
+print(calculator.sub(7, 4))
+print(calculator.mul(2, 4))
+print(calculator.div(27, 3))
